2ee34cf0a060e7d1b2274ba301368572564cb6e6
Simon Sapin <simon.sapin@exyr.org>
2013-11-30 13:34:19 +0000

    Don’t bother removing valid @charset rules from the output.

--- tinycss2/bytes.py
+++ tinycss2/bytes.py
@@ -13,17 +13,13 @@ def decode_stylesheet_bytes(css_bytes, protocol_encoding=None,
 
     The parameters are the same as :func:`parse_stylesheet_bytes`.
 
-    :returns:
-        A ``(css_string, valid_at_charset)`` tuple.
-        :obj:`css_string` is an Unicode string.
-        :obj:`valid_at_charset` is a boolean,
-        true if the encoding was detected based on an ``@charset`` rule.
+    :returns: an Unicode string.
 
     """
     # http://dev.w3.org/csswg/css-syntax/#the-input-byte-stream
     fallback = lookup(protocol_encoding)
     if fallback:
-        return decode(css_bytes, fallback), False
+        return decode(css_bytes, fallback)
     if css_bytes.startswith(b'@charset "'):
         # 10 is len(b'@charset "')
         # 100 is abitrary so that no encoding label is more than 100-10 bytes.
@@ -32,20 +28,20 @@ def decode_stylesheet_bytes(css_bytes, protocol_encoding=None,
             fallback = lookup(css_bytes[10:end_quote])
             if fallback:
                 if fallback.name in ('utf-16be', 'utf-16le'):
-                    return decode(css_bytes, UTF8), False
-                return decode(css_bytes, fallback), True
+                    return decode(css_bytes, UTF8)
+                return decode(css_bytes, fallback)
     fallback = lookup(link_encoding)
     if fallback:
-        return decode(css_bytes, fallback), False
+        return decode(css_bytes, fallback)
     fallback = lookup(document_encoding)
     if fallback:
-        return decode(css_bytes, fallback), False
-    return decode(css_bytes, UTF8), False
+        return decode(css_bytes, fallback)
+    return decode(css_bytes, UTF8)
 
 
 def parse_stylesheet_bytes(css_bytes, protocol_encoding=None,
                            link_encoding=None, document_encoding=None):
-    """Parse stylesheet form bytes.
+    """Parse stylesheet from bytes.
 
     :param css_bytes: A byte string.
     :param protocol_encoding:
@@ -64,16 +60,6 @@ def parse_stylesheet_bytes(css_bytes, protocol_encoding=None,
         :class:`~tinycss2.ast.AtRule`,
         and :class:`~tinycss2.ast.ParseError` objects.
 
-        Any ``@charset`` rule remaining in the list is invalid.
-
     """
-    css_string, valid_at_charset = decode_stylesheet_bytes(
-        css_bytes, protocol_encoding, link_encoding, document_encoding)
-    tokens = iter(parse_component_value_list(css_string))
-    if valid_at_charset:
-        # Consume the @charset rule.
-        assert next(tokens).type == 'at-keyword'
-        assert next(tokens).type == 'whitespace'
-        assert next(tokens).type == 'string'
-        assert next(tokens) == ';'
-    return parse_stylesheet(tokens)
+    return parse_stylesheet(parse_component_value_list(decode_stylesheet_bytes(
+        css_bytes, protocol_encoding, link_encoding, document_encoding)))
