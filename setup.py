700a1b71a7a957b959fe7b83122b74f4731cbf3a
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2017-09-30 21:47:20 +0200

    Clean setup.py and docs conf

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ from setuptools import setup
 
 VERSION = re.search("VERSION = '([^']+)'", codecs.open(
     os.path.join(os.path.dirname(__file__), 'tinycss2', '__init__.py'),
-    encoding="utf-8",
+    encoding='utf-8',
 ).read().strip()).group(1)
 
 README = codecs.open(
     os.path.join(os.path.dirname(__file__), 'README.rst'),
-    encoding="utf-8",
+    encoding='utf-8',
 ).read()
 
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
@@ -30,6 +30,19 @@ setup(
     license='BSD',
     author='Simon Sapin',
     author_email='simon.sapin@exyr.org',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing',
+    ],
     packages=['tinycss2'],
     install_requires=['webencodings>=0.4'],
     package_data={'tinycss2': ['css-parsing-tests/*']},
