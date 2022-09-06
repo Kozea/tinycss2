4dd710da074b1289ad99130da261834c6a2c61cd
Guillaume Ayoub <guillaume@courtbouillon.org>
2022-09-06 15:25:34 +0200

    Clean and improve color4 support
    - clean code and docstrings
    - handle angles in hwb()
    - copy extended colors to avoid a modification of color3’s list

--- tinycss2/color4.py
+++ tinycss2/color4.py
@@ -1,4 +1,5 @@
 from colorsys import hls_to_rgb
+from math import tau
 
 from .color3 import (
     _BASIC_COLOR_KEYWORDS, _EXTENDED_COLOR_KEYWORDS, _HASH_REGEXPS,
@@ -7,12 +8,14 @@ from .parser import parse_one_component_value
 
 
 def parse_color(input):
-    """Parse a color value as defined in `CSS Color Level 4
-    <https://www.w3.org/TR/css-color-4/>`. Implementation of Level 4
-    is currently limited to space-seperated arguments with an optional
-    slash-seperated opacity, definition of 'rebeccapurple',
-    percentages and numbers are accepted as opacity values,
-    the hwb() function and hsla()/rgba() being aliases to hsl()/rgb().
+    """Parse a color value as defined in CSS Color Level 4.
+
+    https://www.w3.org/TR/css-color-4/
+
+    Implementation of Level 4 is currently limited to space-seperated arguments
+    with an optional slash-seperated opacity, definition of 'rebeccapurple',
+    percentages and numbers are accepted as opacity values, the hwb() function,
+    and hsla()/rgba() being aliases to hsl()/rgb().
 
     :type input: :obj:`str` or :term:`iterable`
     :param input: A string or an iterable of :term:`component values`.
@@ -46,47 +49,41 @@ def parse_color(input):
                 return RGBA(*channels)
     elif token.type == 'function':
         args = _parse_separated_args(token.arguments)
-        if args:
+        if args and len(args) in (3, 4):
             name = token.lower_name
-            if name == 'rgb' or name == 'rgba':
-                alpha = _parse_alpha(args[3:])
-                if alpha is not None:
-                    return _parse_rgb(args[:3], alpha)
-                else:
-                    return _parse_rgb(args, alpha=1.)
-            elif name == 'hsl' or name == 'hsla':
-                alpha = _parse_alpha(args[3:])
-                if alpha is not None:
-                    return _parse_hsl(args[:3], alpha)
-                else:
-                    return _parse_hsl(args, alpha=1.)
+            alpha = _parse_alpha(args[3:])
+            if alpha is None:
+                alpha = 1.
+            if name in ('rgb', 'rgba'):
+                return _parse_rgb(args[:3], alpha)
+            elif name in ('hsl', 'hsla'):
+                return _parse_hsl(args[:3], alpha)
             elif name == 'hwb':
-                alpha = _parse_alpha(args[3:])
-                if alpha is not None:
-                    return _parse_hwb(args[:3], alpha)
-                else:
-                    return _parse_hwb(args, alpha=1.)
+                return _parse_hwb(args[:3], alpha)
 
 
 def _parse_separated_args(tokens):
-    """Parse a list of tokens (typically the content of a function token)
-    as arguments made of a single token each, either comma seperated or
-    space-seperated with an optional slash-seperated opacity.
+    """Parse a list of tokens given to a color function.
 
-    return the argument list without commas or white space;
-    or None if the function token content do not match the description above.
+    According to CSS Color Level 4, arguments must be made of a single token
+    each, either comma seperated or space-seperated with an optional
+    slash-seperated opacity.
+
+    Return the argument list without commas or white spaces, or None if the
+    function token content do not match the description above.
 
     """
-    tokens = [token for token in tokens
-              if token.type not in ('whitespace', 'comment')]
+    tokens = [
+        token for token in tokens
+        if token.type not in ('whitespace', 'comment')]
     if len(tokens) % 2 == 1 and all(token == ',' for token in tokens[1::2]):
         return tokens[::2]
     elif len(tokens) == 3 and all(
             token.type in ('number', 'percentage') for token in tokens):
         return tokens
     elif len(tokens) == 5 and tokens[3] == '/':
-        args = [token for token in tokens if token != '/']
-        return args
+        tokens.pop(4)
+        return tokens
 
 
 def _parse_alpha(args):
@@ -96,35 +93,53 @@ def _parse_alpha(args):
     return its value clipped to the 0..1 range. Otherwise, return None.
 
     """
-    if len(args) == 1 and args[0].type == 'number':
-        return min(1, max(0, args[0].value))
-    if len(args) == 1 and args[0].type == 'percentage':
-        return min(1, max(0, args[0].value/100))
+    if len(args) == 1:
+        if args[0].type == 'number':
+            return min(1, max(0, args[0].value))
+        elif args[0].type == 'percentage':
+            return min(1, max(0, args[0].value / 100))
 
 
 def _parse_hwb(args, alpha):
     """Parse a list of HWB channels.
 
-    If args is a list of 1 INTEGER token and 2 PERCENTAGE tokens, return RGB
-    values as a tuple of 3 floats in 0..1. Otherwise, return None.
+    If args is a list of 1 NUMBER or DIMENSION (angle) token and 2 PERCENTAGE
+    tokens, return RGB values as a tuple of 3 floats clipped to the 0..1 range.
+    Otherwise, return None.
 
     """
-    types = [arg.type for arg in args]
-    if types == ['number', 'percentage', 'percentage'] and args[0].is_integer:
-        white = args[1].value / 100
-        black = args[2].value / 100
-        if white + black >= 1:
-            gray = white / (white + black)
-            return RGBA(gray, gray, gray, alpha)
+    if {args[1].type, args[2].type} != {'percentage'}:
+        return
+
+    if args[0].type == 'number':
+        hue = args[0].value / 360
+    elif args[0].type == 'dimension':
+        if args[0].unit == 'deg':
+            hue = args[0].value / 360
+        elif args[0].unit == 'grad':
+            hue = args[0].value / 400
+        elif args[0].unit == 'rad':
+            hue = args[0].value / tau
+        elif args[0].unit == 'turn':
+            hue = args[0].value
         else:
-            rgb = hls_to_rgb(args[0].int_value / 360, 0.5, 1)
-            r, g, b = ((channel * (1 - white - black)) + white
-                       for channel in rgb)
-            return RGBA(r, g, b, alpha)
+            return
+    else:
+        return
+
+    white, black = (arg.value / 100 for arg in args[1:])
+    if white + black >= 1:
+        gray = white / (white + black)
+        return RGBA(gray, gray, gray, alpha)
+    else:
+        rgb = hls_to_rgb(hue, 0.5, 1)
+        r, g, b = ((channel * (1 - white - black)) + white for channel in rgb)
+        return RGBA(r, g, b, alpha)
 
 
 # (r, g, b) in 0..255
-_EXTENDED_COLOR_KEYWORDS.insert(119, ('rebeccapurple', (102, 51, 153)))
+_EXTENDED_COLOR_KEYWORDS = _EXTENDED_COLOR_KEYWORDS.copy()
+_EXTENDED_COLOR_KEYWORDS.append(('rebeccapurple', (102, 51, 153)))
 
 # RGBA named tuples of (r, g, b, a) in 0..1 or a string marker
 _COLOR_KEYWORDS = _SPECIAL_COLOR_KEYWORDS.copy()
