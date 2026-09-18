f761e7f1b256f4ae719eff9e06adeec1a7e1ff9c
Guillaume Ayoub <guillaume@courtbouillon.org>
2026-09-18 11:36:03 +0200

    Improve the an+b serializer and its tests

--- tinycss2/nth.py
+++ tinycss2/nth.py
@@ -76,32 +76,16 @@ def parse_nth(input):
 
 
 def serialize_nth(a, b):
-    """Serialize integer coefficients as a CSS ``<An+B>`` expression.
+    """Serialize `<An+B> <https://drafts.csswg.org/css-syntax/#serializing-anb>`_.
 
-    Follow the `CSS Syntax serialization algorithm
-    <https://drafts.csswg.org/css-syntax/#serializing-anb>`_.
-    The result can be parsed by :func:`parse_nth` to recover ``(a, b)``.
-
-    :type a: :obj:`int`
-    :param a: The step coefficient.
-    :type b: :obj:`int`
-    :param b: The offset.
-    :returns: A :obj:`str`, for example ``'n+3'`` for ``(1, 3)``.
+    :param int a: The step coefficient.
+    :param int b: The offset.
+    :returns: An ``an+b`` string.
 
     """
-    if a == 0:
-        return str(b)
-    if a == 1:
-        result = 'n'
-    elif a == -1:
-        result = '-n'
-    else:
-        result = f'{a}n'
-    if b > 0:
-        result += f'+{b}'
-    elif b < 0:
-        result += str(b)
-    return result
+    an = 'n' if a == 1 else '-n' if a == -1 else f'{a}n' if a else ''
+    b = f'{b}' if not a else f'{b:+}' if b else ''
+    return an + b
 
 
 def parse_b(tokens, a):
