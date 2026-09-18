3b187ad80d89328177667443a954ed7c4efe34bd
Affan Ziya <affan@research.itecz.au>
2026-09-18 10:03:23 +0530

    Fold dimension e-unit escape into a single condition

--- tinycss2/ast.py
+++ tinycss2/ast.py
@@ -568,8 +568,7 @@ class DimensionToken(Node):
         write(self.representation)
         # Disambiguate with scientific notation
         unit = self.unit
-        if unit in ('e', 'E') or unit.startswith(('e-', 'E-')) or (
-                len(unit) >= 2 and unit[0] in 'eE' and unit[1] in '0123456789'):
+        if unit[0] in 'eE' and (len(unit) == 1 or unit[1] in '+-0123456789'):
             write('\\65 ')
             write(serialize_name(unit[1:]))
         else:
